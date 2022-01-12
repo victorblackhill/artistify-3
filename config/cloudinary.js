4ec92d56b08c50cdca34ad2ee163d39f72d8ac4a
Victor Giron <victor.giron@blackhill.dev>
2022-01-12 15:15:45 +0100

    user model

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -3,16 +3,18 @@ const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const multer = require("multer");
 
 // giving access to your cloudinary account
-cloudinary.config({
+const res =cloudinary.config({
   cloud_name: process.env.CLOUDINARY_NAME,
   api_key: process.env.CLOUDINARY_KEY,
   api_secret: process.env.CLOUDINARY_SECRET,
 });
 
+
 // cloudinary : SAAS platform : specialized in images hosting (tools : metadata, image analyzing ...)
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary
 });
+
 
 const fileUploader = multer({ storage });
 // a middleware designed to parse file from requests and associate to req.file
